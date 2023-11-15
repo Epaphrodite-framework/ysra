8436348cb92148a7b8ad0fe8db46b35332ebfec6
Y'srael Aimé N'dri <150064902+Epaphrodite-framework@users.noreply.github.com>
2023-11-15 12:19:32 +0000

    update #add Commentary

--- bin/epaphrodite/define/config/traits/currentSetGuardSecure.php
+++ bin/epaphrodite/define/config/traits/currentSetGuardSecure.php
@@ -6,24 +6,28 @@ trait currentSetGuardSecure
 {
 
     /**
-     * First Hash method
+     * First Hash method (using PASSWORD_DEFAULT constant)
      */
     protected static $Algorithm = PASSWORD_DEFAULT;
 
     /**
-     * Second Hash method
-     */    
+     * Second Hash method (using GOST algorithm)
+     */
     protected static $Gost = 'gost';
 
-
+    /**
+     * Encryption method using OpenSSL (AES-256-CBC)
+     */
     protected static $Openssl = 'AES-256-CBC';
 
-
+    /**
+     * Key used for encryption and decryption (Base64 encoded)
+     */
     protected static $GuardKeys = '5v5Hd+vXmx8fkm4VOr8mm0NiQnU2d1Q2SGdMRW9yUzZXRytGcWc9PQ==';
 
     /**
-     * Latter for security crypt
-     */    
+     * Characters used for generating security-related strings
+     */
     protected static $Guardlatters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
 }
--- bin/epaphrodite/define/config/traits/currentStaticArray.php
+++ bin/epaphrodite/define/config/traits/currentStaticArray.php
@@ -2,21 +2,43 @@
 
 namespace bin\epaphrodite\define\config\traits;
 
-trait currentStaticArray{
+trait currentStaticArray {
 
     /**
+     * List of allowed email domains
+     *
+     * @var array
      * @return array
-    */    
+     */
     protected static $EmailDomaine = ['gmail.com', 'yahoo.com', 'hotmail.com'];
 
     /**
+     * List of allowed file extensions
+     *
+     * @var array
      * @return array
-    */
-    protected static $AllExtensions = [ 'ods','csv','xls','xlsx' ];
+     */
+    protected static $AllExtensions = ['ods', 'csv', 'xls', 'xlsx'];
 
     /**
+     * List of allowed file MIME types
+     *
+     * @var array
      * @return array
-    */
-    protected static $FilesMimes = ['text/x-comma-separated-values', 'text/comma-separated-values', 'application/octet-stream', 'application/vnd.ms-excel', 'application/x-csv', 'text/x-csv', 'text/csv', 'application/csv', 'application/excel', 'application/vnd.msexcel', 'text/plain', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'];
-    
+     */
+    protected static $FilesMimes = [
+        'text/x-comma-separated-values',
+        'text/comma-separated-values',
+        'application/octet-stream',
+        'application/vnd.ms-excel',
+        'application/x-csv',
+        'text/x-csv',
+        'text/csv',
+        'application/csv',
+        'application/excel',
+        'application/vnd.msexcel',
+        'text/plain',
+        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    ];
+
 }
