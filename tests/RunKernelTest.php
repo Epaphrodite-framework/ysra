b57cf2af40356ebcf3ce38972b77437fdab1aac2
Y'srael Aimé N'dri <150064902+Epaphrodite-framework@users.noreply.github.com>
2023-11-24 19:03:06 +0000

    update #Kernel

--- tests/RunKernelTest.php
+++ tests/RunKernelTest.php
@@ -11,19 +11,14 @@ class RunKernelTest extends TestCase
     {
         require_once 'bin/epaphrodite/define/config/SetDirectory.php';
 
-        // Crée une instance de la classe runKernel
         $runKernel = new runKernel();
 
-        // Appelle la méthode Run pour exécuter le code pertinent
         runKernel::Run();
 
-        // Récupère la valeur retournée par getSomeValue()
         $actualValue = $runKernel->getHome();
 
-        // Définit la valeur attendue ici (selon la logique de votre application)
-        $expectedValue = 'views/index/'; // Remplacez ceci par la valeur attendue
+        $expectedValue = 'views/index/';
 
-        // Effectue une assertion pour vérifier si la valeur retournée est égale à la valeur attendue
         $this->assertEquals($expectedValue, $actualValue);
     }
 }
